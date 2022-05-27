ef48328b211b13a11635afc5e741f259484f8cac
Anuj Sharma <anujsharmasppu@gmail.com>
2022-05-28 00:40:17 +0530

    (feat): Add env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TemplatesModule } from './templates/templates.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TemplatesModule],
+  imports: [ConfigModule.forRoot(), TemplatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
