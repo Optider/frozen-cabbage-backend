893ea4f9de429c49d9541426c8b61bbf72bb16c7
Anuj Sharma <anujsharmasppu@gmail.com>
2022-05-28 00:05:07 +0530

    (feat): Add boilerplate download API

--- src/templates/templates.controller.ts
+++ src/templates/templates.controller.ts
@@ -1,11 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, StreamableFile, Response } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, StreamableFile, Response, Query, HttpException, HttpStatus } from '@nestjs/common';
 import { TemplatesService } from './templates.service';
 import { CreateTemplateDto } from './dto/create-template.dto';
 import { UpdateTemplateDto } from './dto/update-template.dto';
+import { createReadStream, existsSync } from 'fs';
+import { join } from 'path';
 
 @Controller('templates')
 export class TemplatesController {
   constructor(private readonly templatesService: TemplatesService) {}
+  
+  @Get('download')
+  getBoilerplate(
+    @Response({ passthrough: true }) res,
+    @Query('backend') backend?: string,
+    @Query('frontend') frontend?: string,
+  ): StreamableFile {
+    try {
+      if (!backend && !frontend)
+        throw new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: 'Both query params cannot be empty',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+
+      const toDownload: string = backend ? 'backend' : 'frontend';
+      const stack: string = backend? backend: frontend;
+
+      // path for boilerplate file
+      const stackFileName = stack + '.zip';
+      const pathStackFile = join(process.cwd(), `boilerplates/${toDownload}`, stackFileName);
+      if (!existsSync(pathStackFile)) {
+        throw new HttpException(
+          {
+            status: HttpStatus.NOT_FOUND,
+            error: `The stack ${stack} is currenlty not available`,
+          },
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      const boilerplate = createReadStream(pathStackFile);
+      res.set({
+        'Content-Type': 'application/zip',
+        'Content-Disposition': `attachment; filename="${stack}.zip"`,
+      });
+      
+      return new StreamableFile(boilerplate);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 
   @Post()
   create(@Body() createTemplateDto: CreateTemplateDto) {
